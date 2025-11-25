c7b3273a271af94bdf156bf4b2a2d46965a93980
Anshuman Naithani <75744157+Anshu0707@users.noreply.github.com>
2025-11-25 09:39:32 +0000

    Add new file

---
+++ 3_Gradient_descent/gradient_descent.py
@@ -0,0 +1,95 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "462c05c7",
+   "metadata": {
+    "vscode": {
+     "languageId": "plaintext"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "import numpy as np"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bd54dd0f",
+   "metadata": {
+    "vscode": {
+     "languageId": "plaintext"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "def gradient_descent(x,y):\n",
+    "  m_curr = c_curr = 0   # Linear equation --> y = mx + c\n",
+    "  learning_rate = 0.08\n",
+    "  n = len(x)        # Number of terms in dependent variable set\n",
+    "  iterations = 1000;         #Maximum number of steps\n",
+    "  \n",
+    "  plt.scatter(x, y, color='red', marker='+', lindeWidth='5')\n",
+    "\n",
+    "  for i in range(iterations):\n",
+    "      y_predicted = c_curr + m_curr * x\n",
+    "      cost = (1/n) * sum([val**2 for val in (y-y_predicted)])\n",
+    "      m_curr_d = -(2/n)*sum(x*(y-y_predicted))\n",
+    "      c_curr_d = -(2/n)*sum(y-y_predicted)\n",
+    "      m_curr = m_curr - (learning_rate * m_curr_d)\n",
+    "      c_curr = c_curr - (learning_rate * c_curr_d)\n",
+    "      print (\"m {}, b {}, cost {} iteration {}\".format(m_curr,c_curr,cost, i))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3b1c7564",
+   "metadata": {
+    "vscode": {
+     "languageId": "plaintext"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "x = np.array([1, 2, 3, 4, 5])\n",
+    "y = np.array([5, 7, 9, 11, 13])"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7beb4c96",
+   "metadata": {
+    "vscode": {
+     "languageId": "plaintext"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "gradient_descent(x,y)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "87aaec0f",
+   "metadata": {
+    "vscode": {
+     "languageId": "plaintext"
+    }
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
